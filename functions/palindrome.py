0b0100fccd95cb66e4c1512592b8617cd8be2f26
Muhammad Umer Farooq <umerfarooq1174@gmail.com>
2026-07-08 23:49:21 +0500

    Palindrome code

---
+++ functions/palindrome.py
@@ -0,0 +1,13 @@
+def isPalindrome(text):
+    lpos=0
+    rpos= len(text)-1
+    while lpos <= rpos:
+        if text[lpos] != text[rpos]:
+            return False
+        lpos=lpos+1
+        rpos=rpos-1
+    return True
+
+print(isPalindrome("madem"))
+txt="madam"
+print(txt==txt[::-1])
